19f43856943a64cd634f139de4111a656cba3cc9
sorahashiroi <sorachansiroi124@gmail.com>
2025-05-03 20:39:37 +0900

    chore: run gofmt to format code

--- cmd/main/pomodomu_test.go
+++ cmd/main/pomodomu_test.go
@@ -3,13 +3,13 @@ package main
 import "testing"
 
 func Example_pomodomu() {
-		goMain([]string{"pomodomu"})
+	goMain([]string{"pomodomu"})
 }
 
 func TestHello(t *testing.T) {
-		got := hello()
-		want := "Welcome to Pomodomu!"
-		if got != want {
-				t.Errorf("hello() = %q, want %q", got, want)
-		}
+	got := hello()
+	want := "Welcome to Pomodomu!"
+	if got != want {
+		t.Errorf("hello() = %q, want %q", got, want)
+	}
 }
