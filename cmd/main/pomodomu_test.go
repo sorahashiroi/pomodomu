f3a11626e9c45f27842c73402186348138e4b1ea
sorahashiroi <sorachansiroi124@gmail.com>
2025-05-07 23:15:23 +0900

    update go files

--- cmd/main/pomodomu_test.go
+++ cmd/main/pomodomu_test.go
@@ -3,15 +3,15 @@ package main
 import "testing"
 
 func Example_pomodomu() {
-	goMain([]string{"pomodomu"})
-	// Output:
-	// Welcome to Pomodomu!
+        goMain([]string{"pomodomu"})
+        // Output:
+        // Welcome to Pomodomu!
 }
 
 func TestHello(t *testing.T) {
-	got := hello()
-	want := "Welcome to Pomodomu!"
-	if got != want {
-		t.Errorf("hello() = %q, want %q", got, want)
-	}
+        got := hello()
+        want := "Welcome to Pomodomu!"
+        if got != want {
+                t.Errorf("hello() = %q, want %q", got, want)
+        }
 }
